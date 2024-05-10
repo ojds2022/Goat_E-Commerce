7689f994159bbf2fd551effb861e08d347b061ce
RApostolJR <romuloapostol@gmail.com>
2024-05-09 20:24:19 -0700

    added productDetailsRoutes, changed console log in server.js for easier access to localhost

--- Main/controllers/index.js
+++ Main/controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
+const productDetailRoutes = require('./productDetailRoutes')
 
 router.use('/', homeRoutes);
+router.use('/productDetailRoutes', productDetailRoutes);
 
 module.exports = router;

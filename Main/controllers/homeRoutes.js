d68ccac3bf544201f6e4bdd504f2e3ad28f1b94a
Felix Teng <fsmunozteng@gmail.com>
2024-05-10 21:12:19 -0700

    added login get fetch code for login inputs in file homeRoute.js under controller

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { Items } = require('../models');
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
 
 
 router.get('/',  async (req, res) => {
@@ -15,6 +17,36 @@ router.get('/',  async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+// Prevent non logged in users from viewing the homepage
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.render('homepage', {
+      users,
+      // Pass the logged in flag to the template
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If a session exists, redirect the request to the homepage
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;

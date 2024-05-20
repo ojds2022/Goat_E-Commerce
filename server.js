8cb64a3762244a4009642d198aa5566365509f76
GW <garykachunwu@gmail.com>
2024-05-19 22:28:16 -0700

    bruh

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-
+const {sessionMiddleware} = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();

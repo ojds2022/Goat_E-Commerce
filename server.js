236e770caf244b9111613c2b9dcf51b77183329f
ojds2022 <odiazstrandberg@gmail.com>
2024-05-20 13:33:57 -0700

    fix: updated server.js file to have process.env.SECRET

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create();
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SECRET,
 
 cookie: {
   maxAge: 24 * 60 * 60 * 1000,

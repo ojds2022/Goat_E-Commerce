6142aec5d2da874df1f283dc30c4e9b0dfde845b
GW <garykachunwu@gmail.com>
2024-05-19 19:14:20 -0700

    ff

--- Main/config/connection.js
+++ Main/config/connection.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
+// require('dotenv').config();
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize("mysql://hr3628rpuym7my35:uuy8f2t9xl8yknpe@lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaw");
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
